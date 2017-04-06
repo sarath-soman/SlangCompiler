cdff4059b9957303fcd03c82e5c0a06fc50d7fd3
Sarath <nsarathsoman@gmail.com>
2017-04-07 00:19:17 +0530

    slangc initial

--- src/main/java/com/slang/SlangC.java
+++ src/main/java/com/slang/SlangC.java
@@ -1,11 +1,13 @@
 package com.slang;
 
+import java.util.Arrays;
+
 /**
  * Created by sarath on 16/3/17.
  */
-public class Main {
+public class SlangC {
 
     public static void main(String[] args) {
-
+        Arrays.stream(args).forEach(s -> System.out.println(s));
     }
 }
